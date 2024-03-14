1002cb37a682dc9e7b7dff83183973696c3f57d3
luiz <luiz_tauffer@hotmail.com>
2024-03-14 18:54:48 +0100

    contexts

--- si_kilosort25/test_in_container.sh
+++ si_kilosort25/test_in_container.sh
@@ -5,12 +5,13 @@ IMAGE="ghcr.io/catalystneuro/dendro_si_kilosort25"
 
 # Command to be executed inside the container
 ENTRYPOINT_CMD="dendro"
-ARGS="test-app-processor --app-dir . --processor spikeinterface_pipeline_ks25 --context sample_context_1.yaml"
+ARGS="test-app-processor --app-dir . --processor spikeinterface_pipeline_ks25 --context sample_context_2.yaml"
 
 
-# Run the Docker container
+# Run the Docker container, with conveninent volumes
 docker run --gpus all \
-    -v $(pwd)/sample_context_1.yaml:/app/sample_context_1.yaml \
+    -v $(pwd)/results:/app/output \
+    -v $(pwd)/sample_context_2.yaml:/app/sample_context_2.yaml \
     -v /mnt/shared_storage/Github/si-dendro-apps/common:/app/common \
     -v /mnt/shared_storage/Github/dendro/python:/src/dendro/python \
     -v /mnt/shared_storage/Github/spikeinterface_pipelines:/src/spikeinterface_pipelines \
