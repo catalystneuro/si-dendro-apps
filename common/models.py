03f66fa0fe4c65ba0407d3b43139e9d6819ee150
luiz <luiz_tauffer@hotmail.com>
2024-04-01 13:00:22 +0200

    visualization

--- common/models.py
+++ common/models.py
@@ -5,6 +5,7 @@ from typing import Union
 from .models_preprocessing import PreprocessingContext
 from .models_postprocessing import PostprocessingContext
 from .models_curation import CurationContext
+from .models_visualization import VisualizationContext
 from .models_sorting import (
     Kilosort25SortingContext,
     Kilosort3SortingContext,
@@ -19,6 +20,7 @@ class RecordingContext(BaseModel):
     write_recording: bool = Field(default=False, description='Write recording')
     stub_test: bool = Field(default=False, description='Stub test')
     stub_test_duration_sec: float = Field(default=300, description='Stub test duration in seconds')
+    stub_test_num_channels: int = Field(default=-1, description='Stub test number of channels')
 
 
 class JobKwargs(BaseModel):
@@ -62,42 +64,4 @@ class PipelineFullContext(BaseModel):
     run_curation: bool = Field(default=True, description='Run curation')
     curation_context: CurationContext = Field(default=CurationContext(), description='Curation context')
     run_visualization: bool = Field(default=True, description='Run visualization')
-    # visualization_context: VisualizationContext = Field(default=VisualizationContext(), description='Visualization context')
-
-
-# # ------------------------------
-# # Visualization Models
-# # ------------------------------
-# class Timeseries:
-#     n_snippets_per_segment: int = Field(2, description="Number of snippets per segment")
-#     snippet_duration_s: float = Field(0.5, description="Duration of the snippet in seconds")
-#     skip: bool = Field(False, description="Flag to skip")
-
-
-# class Detection:
-#     method: str = Field("locally_exclusive", description="Method for detection")
-#     peak_sign: str = Field("neg", description="Sign of the peak")
-#     detect_threshold: int = Field(5, description="Detection threshold")
-#     exclude_sweep_ms: float = Field(0.1, description="Exclude sweep in milliseconds")
-
-
-# class Localization:
-#     ms_before: float = Field(0.1, description="Milliseconds before")
-#     ms_after: float = Field(0.3, description="Milliseconds after")
-#     local_radius_um: float = Field(100.0, description="Local radius in micrometers")
-
-
-# class Drift:
-#     detection: Detection
-#     localization: Localization
-#     n_skip: int = Field(30, description="Number of skips")
-#     alpha: float = Field(0.15, description="Alpha value")
-#     vmin: int = Field(-200, description="Minimum value")
-#     vmax: int = Field(0, description="Maximum value")
-#     cmap: str = Field("Greys_r", description="Colormap")
-#     figsize: Tuple[int, int] = Field((10, 10), description="Figure size")
-
-
-# class VisualizationKwargs:
-#     timeseries: Timeseries
-#     drift: Drift
+    visualization_context: VisualizationContext = Field(default=VisualizationContext(), description='Visualization context')
